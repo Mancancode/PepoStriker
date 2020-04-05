a8ffe1679761796057aff246ac02d5f7572bb268
mancancode <madebymaniq@live.com>
2020-04-05 20:30:15 +0100

    fix para update

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const hamburger = document.getElementById('hamburgerBtn');
 const nav = document.getElementById('nav');
 
 hamburgerBtn.addEventListener('click',( )=>{
-    
     nav.classList.toggle('open');
 
     });
